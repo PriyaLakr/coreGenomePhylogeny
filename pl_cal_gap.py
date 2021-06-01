5614d5dae858dc6e1325d5c86dcf9c996664aaeb
PriyaLakra <66781257+PriyaLakr@users.noreply.github.com>
2021-06-01 19:26:14 +0530

    Update pl_cal_gap.py

--- pl_cal_gap.py
+++ pl_cal_gap.py
@@ -41,14 +41,14 @@ if __name__ == "__main__":
 
 	parser = argparse.ArgumentParser(description = "This script calculates gaps in the multiple sequence alignment") 
 	parser.add_argument("--path", type=str, help="Path of the directory where alignment files are stored")
-    parser.add_argument("--suffix", type=str, help="Suffix of the alignment files")
+	parser.add_argument("--suffix", type=str, help="Suffix of the alignment files")
     
-    print("Input files should be aligned first")
+    	print("Input files should be aligned first")
 
-    args = parser.parse_args()
-    file_path = args.path
-    in_format = args.suffix
+    	args = parser.parse_args()
+    	file_path = args.path
+    	in_format = args.suffix
 
-    cal_gaps_align(file_path,in_format)
+    	cal_gaps_align(file_path,in_format)
 
 
