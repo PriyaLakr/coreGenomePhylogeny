e362cdcf618bdd33d310148f6c7891680b8e4143
PriyaLakra <PriyaLakr@users.noreply.github.com>
2021-06-23 20:58:54 +0530

    some changes

--- pl_install_depend.sh
+++ pl_install_depend.sh
@@ -2,17 +2,17 @@
 
 ## Installing dependencies with conda 
 
-conda create -n core_genome_phylo
+conda create -n core_genome_phylo | tee -a install.info
 
-conda activate core_genome_phylo
+source activate core_genome_phylo && echo "Env activated" | tee -a  install.info
 
-conda install -c bioconda mafft
+conda install -c bioconda mafft | tee -a install.info 
 
-conda install -c bioconda iqtree
+conda install -c bioconda iqtree | tee -a install.info
 
 conda deactivate 
 
-echo "Dependencies installation complete!"
-echo "Before starting the analysis, always run first: conda activate <enviornment_name> ; eg. conda activate core_genome_phylo"
+echo -e "\nDependencies installation complete!" 
+echo -e "\nBefore starting the analysis, always run first: conda activate <enviornment_name> ; eg. conda activate core_genome_phylo\n"
 
 
