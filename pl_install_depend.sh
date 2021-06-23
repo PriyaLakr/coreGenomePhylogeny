5176cc90d83f3b06d81e34323c2c11ac7ea032ee
PriyaLakra <PriyaLakr@users.noreply.github.com>
2021-06-23 21:07:18 +0530

    latest

--- pl_install_depend.sh
+++ pl_install_depend.sh
@@ -4,13 +4,13 @@
 
 conda create -n core_genome_phylo | tee -a install.info
 
-source activate core_genome_phylo && echo "Env activated" | tee -a  install.info
+source activate core_genome_phylo && echo -e "\nEnv activated\n" | tee -a install.info
 
 conda install -c bioconda mafft | tee -a install.info 
 
 conda install -c bioconda iqtree | tee -a install.info
 
-conda deactivate 
+conda deactivate && echo -e "\nEnv deactivated\n" | tee -a install.info 
 
 echo -e "\nDependencies installation complete!" 
 echo -e "\nBefore starting the analysis, always run first: conda activate <enviornment_name> ; eg. conda activate core_genome_phylo\n"
