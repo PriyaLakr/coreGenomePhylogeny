ed4e286ab1d3f8de5ecedd40d3a8710c6de2b88a
PriyaLakra <PriyaLakr@users.noreply.github.com>
2021-06-28 23:52:45 +0530

    update

--- pl_install_depend.sh
+++ pl_install_depend.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+present_dir="$PWD"
+
+## install get_homologous in your home directory; make sure you have internet connection to execute the download properly 
+get_homo(){
+	git clone https://github.com/eead-csic-compbio/get_homologues.git 
+	cd get_homologues
+	perl install.pl && \
+	echo -e "Get homologoues software downloaded in your home directory: "$PWD" " 
+}
+
 ## Installing dependencies with conda 
 
 conda create -n core_genome_phylo | tee -a install.info
@@ -12,7 +22,13 @@ conda install -c bioconda iqtree | tee -a install.info
 
 conda deactivate && echo -e "\nEnv deactivated\n" | tee -a install.info 
 
-echo -e "\nDependencies installation complete!" 
-echo -e "\nBefore starting the analysis, always run first: conda activate <enviornment_name> ; eg. conda activate core_genome_phylo\n"
+cd ~
+get_homo 
+cd "$present_dir"
+
+echo -e "\nDependencies installation complete!" | tee -a install.info 
+echo -e "\nBefore starting the analysis, always run first: conda activate <enviornment_name> ; eg. conda activate core_genome_phylo\n" | tee -a install.info 
+
+
 
 
